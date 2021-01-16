4a0dbb6e502eb55709123a237dc0c15fadd81c70
webninjaafrica <38395056+webninjaafrica@users.noreply.github.com>
2021-01-16 05:01:43 -0800

    Add files via upload

---
+++ dep.py
@@ -0,0 +1,281 @@
+from flask import Flask
+
+
+app2=Flask(__name__)
+
+
+@app2.route('/')
+def depl():
+    return '''
+
+
+{
+  "result": [
+    {
+      "batch": "201908695", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "python", 
+      "id": 1, 
+      "name": "abishek sigh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909298", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 2, 
+      "name": "bishnei karandep", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201907489", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "python", 
+      "id": 3, 
+      "name": "Harjat sigh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909522", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 4, 
+      "name": "harmandeep sig", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909309", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "python", 
+      "id": 5, 
+      "name": "kaur karamjet", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909099", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 6, 
+      "name": "khan , imran", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "20190915", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "python", 
+      "id": 7, 
+      "name": "Khangura, kurveeh sigh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909617", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 8, 
+      "name": "pahwa, harsh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908968", 
+      "branch": "A", 
+      "college": "AMC", 
+      "course": "IT", 
+      "first_language": "C++", 
+      "id": 9, 
+      "name": "patel divyaben", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908875", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "Lisp", 
+      "id": 10, 
+      "name": "patel, vishaben", 
+      "program": "IT"
+    }, 
+    {
+      "batch": "201909349", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "CS", 
+      "first_language": "PHP", 
+      "id": 11, 
+      "name": "rajinder, kumar", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909052", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "c#", 
+      "id": 12, 
+      "name": "rakshat kumar, rakshat kumar", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909141", 
+      "branch": "A", 
+      "college": "BC", 
+      "course": "CT", 
+      "first_language": "JAVASCRIPT", 
+      "id": 13, 
+      "name": "sachdeva anubhav", 
+      "program": "Pogramming"
+    }, 
+    {
+      "batch": "201909142", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "NODE.JS", 
+      "id": 14, 
+      "name": "sandtha anmor sig", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908449", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "LUA", 
+      "id": 15, 
+      "name": "shah dashrya", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908813", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "CS", 
+      "first_language": "COBOL", 
+      "id": 16, 
+      "name": "sharma , karan", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908778", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "python", 
+      "id": 17, 
+      "name": "sindhu papinder, sigh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201908841", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 18, 
+      "name": "sigh jasmeet", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909103", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C#", 
+      "id": 19, 
+      "name": "sigh, manoreet", 
+      "program": "OOP"
+    }, 
+    {
+      "batch": "201909449", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "F#", 
+      "id": 20, 
+      "name": "Tanvir Sign, Tanvir Sigh", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909263", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "D#", 
+      "id": 21, 
+      "name": "Thind AnchaPreet, Kaur", 
+      "program": "System Analysis"
+    }, 
+    {
+      "batch": "201908080", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C", 
+      "id": 22, 
+      "name": "Tumat, Premsati", 
+      "program": "ICS"
+    }, 
+    {
+      "batch": "201909373", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C++", 
+      "id": 23, 
+      "name": "Vikramjeet Sigh", 
+      "program": "ICT"
+    }, 
+    {
+      "batch": "201908874", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "C#", 
+      "id": 24, 
+      "name": "Vikrant, Vikrant", 
+      "program": "BI"
+    }, 
+    {
+      "batch": "201909150", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "PHP", 
+      "id": 25, 
+      "name": "Vonugranti, Praveen", 
+      "program": "Programming"
+    }, 
+    {
+      "batch": "201909150", 
+      "branch": "A", 
+      "college": "ABC", 
+      "course": "IT", 
+      "first_language": "PHP", 
+      "id": 26, 
+      "name": "Vonugranti, Praveen", 
+      "program": "ICT"
+    }
+  ]
+}
+'''
+
+
+if __name__=="__main__":
+    app2.run(debug=True)
+    
